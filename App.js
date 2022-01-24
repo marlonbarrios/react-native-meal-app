3beacd4d41c52c0cf2329c284e131ab9b7bf3490
marlonbarrios <marlon@dance-tech.net>
2022-01-24 16:10:02 -0500

    app with bottom tabs

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View,  } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font'
+import React,{ useState } from 'react';
+import MealsNavigator from './navigation/MealsNavigator';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+    startAsync={fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    onError={(err) => console.log(err)}
+/>
+    );
+  }
+
+  return <MealsNavigator />;
+}
